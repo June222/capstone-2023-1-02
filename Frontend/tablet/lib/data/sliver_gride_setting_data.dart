d4b3d496806b93227b5c7c6b87409ca148568738
June222 <38900003+June222@users.noreply.github.com>
2023-10-30 17:25:38 +0900

    Add grid settings
    GridView의 설정이 저장된 파일.

---
+++ Frontend/tablet/lib/data/sliver_gride_setting_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const SliverGridDelegateWithFixedCrossAxisCount sliverGridSetting =
+    SliverGridDelegateWithFixedCrossAxisCount(
+  crossAxisCount: 3,
+  childAspectRatio: 0.8,
+  mainAxisSpacing: 30,
+  crossAxisSpacing: 20,
+);
